f2ba6806090977e1896130eb94c16f5e245ba592
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-05-06 20:46:00 -0500

    Add endpoints of services, orders and authentication

--- src/main/java/com/litografiaartesplanchas/application/gateway/GatewayConfig.java
+++ src/main/java/com/litografiaartesplanchas/application/gateway/GatewayConfig.java
@@ -12,6 +12,9 @@ public class GatewayConfig {
 	private int clientsPort = 8080;
 	private int inventoryPort = 8081;
 	private int employeePort = 8082;
+	private int servicePort = 8083;
+	private int orderPort = 8084;
+	private int authPort = 8085; 
 	
 	private String basePath = "http://localhost";
 	
@@ -33,6 +36,18 @@ public class GatewayConfig {
                         .path("/api/v1/employee/**")
                         .filters(f -> f.addRequestHeader("Content-Type", "application/json"))
                         .uri(String.format("%s:%d", basePath, employeePort)))
+                .route("service", p -> p
+                        .path("/api/v1/service/**")
+                        .filters(f -> f.addRequestHeader("Content-Type", "application/json"))
+                        .uri(String.format("%s:%d", basePath, servicePort)))
+                .route("order", p -> p
+                        .path("/api/v1/orders/**")
+                        .filters(f -> f.addRequestHeader("Content-Type", "application/json"))
+                        .uri(String.format("%s:%d", basePath, orderPort)))
+                .route("auth", p -> p
+                        .path("/auth/**")
+                        .filters(f -> f.addRequestHeader("Content-Type", "application/json"))
+                        .uri(String.format("%s:%d", basePath, authPort)))
                     .build();
     }
 }
